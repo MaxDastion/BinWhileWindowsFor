ae6cae9e90f2d2a77e5ef1b9585dc423bee46721
Студент <@3-8>
2023-11-23 19:19:49 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.TrashBack = new System.Windows.Forms.PictureBox();
+            this.TimerTrashback = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.TrashBack)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TrashBack
+            // 
+            this.TrashBack.Image = ((System.Drawing.Image)(resources.GetObject("TrashBack.Image")));
+            this.TrashBack.Location = new System.Drawing.Point(1, 350);
+            this.TrashBack.Name = "TrashBack";
+            this.TrashBack.Size = new System.Drawing.Size(100, 100);
+            this.TrashBack.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.TrashBack.TabIndex = 0;
+            this.TrashBack.TabStop = false;
+            // 
+            // TimerTrashback
+            // 
+            this.TimerTrashback.Enabled = true;
+            this.TimerTrashback.Tick += new System.EventHandler(this.MoveTrashbackivent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TrashBack);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyisDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyisUp);
+            this.Resize += new System.EventHandler(this.ResizeEvent);
+            ((System.ComponentModel.ISupportInitialize)(this.TrashBack)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox TrashBack;
+        private System.Windows.Forms.Timer TimerTrashback;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+
+        bool _moveRight, _moveLeft;
+        int _LastWidh = 0;
+
+        int speed = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void MoveTrashbackivent(object sender, EventArgs e)
+        {
+            _LastWidh = this.Size.Width;
+            speed = this.Size.Width / 2 - 50;
+            if (_moveLeft == true && TrashBack.Left > 0)
+            {
+                if (!(TrashBack.Left - speed < 1))
+                {
+                    TrashBack.Left -= speed;
+                }
+            }
+            if (_moveRight == true && TrashBack.Right < this.Width -50)
+            {
+                if (!(TrashBack.Left + speed > this.Width - 50))
+                {
+                    TrashBack.Left += speed;
+                }
+            }
+
+        }
+
+        private void KeyisDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                _moveLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                _moveRight = true;
+            }
+        }
+
+        private void ResizeEvent(object sender, EventArgs e)
+        {
+            if (TrashBack.Left == 1)
+            {
+
+            TrashBack.Left = 1;
+            }
+            else if (TrashBack.Left == this._LastWidh /2)
+            {
+                TrashBack.Left = this.Width  /2  ;
+            }
+            else if (TrashBack.Left == this._LastWidh -50)
+            {
+                TrashBack.Left = this.Width -50;
+            }
+            TrashBack.Top = this.Height - 130;
+            
+           
+
+        }
+
+        private void KeyisUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                _moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                _moveRight = false;
+            }
+        }
+    }
+}
